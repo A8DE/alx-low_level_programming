8709a36e788d4567aad6896353b8fa2d34df30e3
A8DE <abdssamad.gourchi@gmail.com>
2023-05-02 10:38:46 -0700

    2-add_node File

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,19 +10,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *newNode = malloc(sizeof(list_t));
-    unsigned int len = 0;
+    int l = 0;
 
-    while (str[len])
-    len++;
+    while (str[l])
+    l++;
 
     if (!newNode)
 	return (NULL);
 
     newNode->str = strdup(str);
-    newNode->len = len;
+    newNode->len = l;
     newNode->next = *head;
     *head = newNode;
 
     return (*head);
 }
-
