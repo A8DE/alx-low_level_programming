069d4a3c7bcb7957d8f9d81c9ebb434fa318d8ec
A8DE <abdssamad.gourchi@gmail.com>
2023-05-02 14:27:56 -0700

    creat 101-hello_holberton File

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
 global   main
 	  extern    printf
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+	mov   edi, Msg
+	xor   eax, eax
+	call  printf
+	mov   eax, 0
+	ret
+Msg: db `Hello, Holberton\n`,0
 
