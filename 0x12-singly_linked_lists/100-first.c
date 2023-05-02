1b1e90a858e881c605ca0914cb47106bf8f07fb7
A8DE <abdssamad.gourchi@gmail.com>
2023-05-02 14:02:05 -0700

    Creat 100-first file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-void first(void) __attribute__ ((constructor));
-
+void beforMain(void) __attribute__ ((constructor));
 /**
- * first - prints a sentence before the main
- * function is executed
+ * beforMain - Function  prints a sentence before the main function is executed
  */
-void first(void)
+void beforMain(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
