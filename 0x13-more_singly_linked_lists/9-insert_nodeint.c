43e2cb98d21e6d2920122407d02771d8043ae97b
A8DE <abdssamad.gourchi@gmail.com>
2023-05-02 19:17:56 -0700

    9-insert_nodeint file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,39 +6,38 @@
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
- *
  * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *newNode;
+	listint_t *t = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (!newNode || !head)
 		return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+	newNode->n = n;
+	newNode->next = NULL;
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; t && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			newNode->next = t->next;
+			t->next = newNode;
+			return (newNode);
 		}
 		else
-			temp = temp->next;
+			t = t->next;
 	}
 
 	return (NULL);
