28a92cbb3bc1bb5cdd66aab9fc34df3e8c9d00a0
A8DE <abdssamad.gourchi@gmail.com>
2023-07-11 05:54:52 -0700

    READ TEXT FILE

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,20 +10,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
+	char *b;
+	ssize_t f;
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	f = open(filename, O_RDONLY);
+	if (f == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	b = malloc(sizeof(char) * letters);
+	t = read(f, b, letters);
+	w = write(STDOUT_FILENO, b, t);
 
-	free(buf);
-	close(fd);
+	free(b);
+	close(f);
 	return (w);
 }
 
