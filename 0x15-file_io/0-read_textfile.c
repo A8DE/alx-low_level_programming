bed946e125761ee38633bed89a7eae799fa9a6b9
A8DE <abdssamad.gourchi@gmail.com>
2023-07-11 04:32:17 -0700

    0 READ TEXT FILE

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - Function print STDOUT
+ * @filename: text file read
+ * @letters: number of letters read
+ * Return: w- actual number bytes read printed
+ * 0 filename NULL function fail
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *b;
+	ssize_t f;
+	ssize_t w;
+	ssize_t t;
+
+	f = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0)
+	b = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(b);
+	close(f);
+	return (w);
+}
