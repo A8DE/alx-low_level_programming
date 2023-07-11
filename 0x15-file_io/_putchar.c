4cf4d5b5e0b5f16424106644727262e73f86cff1
A8DE <abdssamad.gourchi@gmail.com>
2023-07-11 05:36:56 -0700

    PUTCHAR FILE

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes character c stdout
+ * @c: The character to print
+ * Return: success 1.
+ * error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
