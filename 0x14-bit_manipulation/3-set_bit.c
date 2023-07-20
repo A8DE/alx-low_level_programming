62a67ec726411c4edc5502db45b949d1735ce7ba
A8DE <abdssamad.gourchi@gmail.com>
2023-07-20 03:53:44 -0700

    C - Bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - Function Sets Bit Given Index  1
+ * @n: Pointer Number Change
+ * @index: Index Bit Set 
+ * Return: Succcess 1, Failure -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
